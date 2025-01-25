8be7433bf87f008d737f6e45bd0e764fdc63a81d
Ramvilasteku <ramvilasteku@gmail.com>
2025-01-25 17:05:17 +0530

    new

--- main.js
+++ main.js
@@ -1,23 +1,81 @@
-// Load modals dynamically
-document.addEventListener('DOMContentLoaded', () => {
-    // Load Signup Modal
-    fetch('auth/signup/signup.html')
-      .then((response) => response.text())
-      .then((data) => {
-        document.body.insertAdjacentHTML('beforeend', data);
-        const signupScript = document.createElement('script');
-        signupScript.src = 'auth/signup/signup.js';
-        document.body.appendChild(signupScript);
-      });
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAzIRmxl1OQ3TGOlXfxy6JPufnvDr6Okg",
+  authDomain: "vastra-vilas.firebaseapp.com",
+  projectId: "vastra-vilas",
+  storageBucket: "vastra-vilas.firebasestorage.app",
+  messagingSenderId: "429952723766",
+  appId: "1:429952723766:web:1779054f5b388c83536197"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const author=getAuth(app)
+
+
+
+
+let signUpBtn = document.getElementById("signUpBtn");
+
+signUpBtn.addEventListener("click", (e) => {
+  e.preventDefault
+// let signUpModal =document.getElementById("signUpModal")
+// console.log(signUpModal);
+
+let signUpModal =new bootstrap.Modal(document.getElementById("signUpModal"));
+signUpModal.show()
+
+let signupMbtn =document.getElementById("signupMbtn");
+signupMbtn.addEventListener('click',async()=>{
+
+  let signUpName=document.getElementById('signUpName').value.trim();
+  let signUpEmail=document.getElementById('signUpEmail').value.trim();
+  let signUpPassword=document.getElementById('signUpPassword').value.trim();
+  let signUpCpassword=document.getElementById('signUpCpassword').value.trim();
+
+  if( signUpEmail === "" || signUpName==="" || signUpPassword === ""){
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "enter all fields",
+      }).then(()=>{
+        signUpModal.show()
+      })
+      return;
+}
+
+
+try{
+          await createUserWithEmailAndPassword(author,signUpEmail,signUpPassword).then(()=>{
+            Swal.fire({
+                title: "registered successfully!",
+                icon: "success",
+              }).then(()=>{
+                document.getElementById("signUpName").textContent=""
+                document.getElementById("signUpEmail").textContent=""
+                document.getElementById("signUpPassword").textContent=""
+                document.getElementById("signUpCpassword").textContent=""
+                location.href="./main/home.html"
+              })
+          })
+}catch(err){
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: err,
+      })
+}
+
+
+})
+
+
+//    console.log(signUpModal)
+})
+
+
   
-    // Load Login Modal
-    fetch('auth/login/login.html')
-      .then((response) => response.text())
-      .then((data) => {
-        document.body.insertAdjacentHTML('beforeend', data);
-        const loginScript = document.createElement('script');
-        loginScript.src = 'auth/login/login.js';
-        document.body.appendChild(loginScript);
-      });
-  });
-  
+
+
